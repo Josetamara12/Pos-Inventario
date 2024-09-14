25797eeca8fa5e692f0a320a9e2b2b246d167746
haroldCoder <haroldc2005@gmail.com>
2024-09-13 20:57:43 -0500

    configure vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   await new ConnectionDB();
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json())
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
